d738b31e151f365994b1e47271be0773c11a3c8c
Suleman96 <m.suleman.me96@gmail.com>
2026-03-24 11:49:45 +0100

    Refactor employee schemas to enhance data validation and add new fields for employee management

--- schemas.py
+++ schemas.py
@@ -1,39 +1,72 @@
 # His is how data is validated ad passed around
 
-from pydantic import BaseModel, EmailStr
+from datetime import datetime
 from typing import Optional
+
+from pydantic import BaseModel, EmailStr, ConfigDict
+
 
 #This is for incoming data when a user submits a new employee.
 #Optional[str] = None This means the field can be empty.
 
 
-class EmployeeCreate(BaseModel):
+class EmployeeBase(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     date_of_birth: Optional[str] = None
     place_of_birth: Optional[str] = None
-    phone: Optional[str] = None
-    email: Optional[EmailStr] = None
+    country_of_birth: Optional[str] = None
+    nationality: Optional[str] = None
+    
     street_and_number: Optional[str] = None
+    phone: Optional[str] = None
     zip_code: Optional[str] = None
     city: Optional[str] = None
-    country: Optional[str] = "Deutschland"
+    email: Optional[EmailStr] = None
+    county: Optional[str] = None
+    
+    steuer_id: Optional[str] = None
+    steuerklasse: Optional[str] = None
+    iban: Optional[str] = None
+    bic: Optional[str] = None
+    
+    start_date: Optional[str] = None
+    contract_type: Optional[str] = None
+    end_date: Optional[str] = None
+    disabled: Optional[str] = None
+
+    status: Optional[str] = "draft"
+    ordio_id: Optional[str] = None
+    
+    
+    
+class EmployeeCreate(EmployeeBase):
+    pass
+class EmployeeUpdate(EmployeeBase):
+    pass    
     
 #This is for sending employee data back out safely without exposing sensitive information.
 class EmployeeResponse(BaseModel):
     id: int
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    date_of_birth: Optional[str] = None
-    place_of_birth: Optional[str] = None
-    phone: Optional[str] = None
-    email: Optional[EmailStr] = None
-    street_and_number: Optional[str] = None
-    zip_code: Optional[str] = None
-    city: Optional[str] = None
-    country: Optional[str] = None
-    status: str
+    created_at: datetime
+    update_at: datetime 
+    approved_at: Optional[datetime] = None
+    approved_by: Optional[str] = None
+    is_deleted: bool = False
+    deleted_reason: Optional[str] = None
+    # this is for converting the SQLAlchemy model to a Pydantic model. 
+    # It allows us to return SQLAlchemy objects directly from our API endpoints 
+    # and have them automatically converted to the appropriate Pydantic response model.
+    model_config= ConfigDict(from_attributes=True)
     
-    class Config:
-        from_attribute = True
-        
+class AuditLogResponse(BaseModel):
+    id: int
+    action: str
+    employee_id: int
+    details: str
+    performed_by: str
+    timestamp: datetime
+    
+    model_config= ConfigDict(from_attributes=True)
+    
+    
